74825b1fc3256f0d13ec1fb316f814d531359e04
siyujing <siyujing@boxfish.cn>
2018-01-22 17:18:48 +0800

    no message

--- new-datasource-mongo-jpa-demo/src/main/java/com/testingedu/domain/nosql/CourseLabelEntity.java
+++ new-datasource-mongo-jpa-demo/src/main/java/com/testingedu/domain/nosql/CourseLabelEntity.java
@@ -2,12 +2,18 @@ package com.testingedu.domain.nosql;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.Id;
 
 @Data
 @Document(collection = "course_label_longze")
+@CompoundIndexes({
+        @CompoundIndex(name = "index_courseId", def = "{'courseId': 1}", unique = true),
+        @CompoundIndex(name = "index_courseId_courseNameChinese", def = "{'courseId': 1,'courseNameChinese': 1}")
+})
 public class CourseLabelEntity {
     @Id
     @JsonIgnore
