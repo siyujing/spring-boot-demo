1af70e22d9a7d1e139ae42ea692d3d149b7fb986
siyujing <siyujing@boxfish.cn>
2018-01-23 16:27:42 +0800

    no message

---
+++ spring-boot-util-demo/src/main/java/com/testingedu/utilsdemo/time_utils/service/TestDemo.java
@@ -0,0 +1,27 @@
+package com.testingedu.utilsdemo.time_utils.service;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+
+/**
+ * Created by boxfish on 18/1/23.
+ */
+public class TestDemo {
+
+    public static void main(String[] args) {
+        test();
+    }
+
+    public static void test() {
+        Hashtable hashtable = new Hashtable();
+
+        hashtable.put("a", "aa");
+        hashtable.put("b", "bb");
+
+        Enumeration keys = hashtable.keys();
+        while (keys.hasMoreElements()){
+            Object o = keys.nextElement();
+            System.out.println(o+":"+hashtable.get(o));
+        }
+    }
+}
