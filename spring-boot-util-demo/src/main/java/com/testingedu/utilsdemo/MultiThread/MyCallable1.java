6338288bcc8963c739cbb04ecac6a41bf3327f99
siyujing <siyujing@boxfish.cn>
2018-04-28 18:08:02 +0800

    no message

--- spring-boot-util-demo/src/main/java/com/testingedu/utilsdemo/MultiThread/MyCallable1.java
+++ spring-boot-util-demo/src/main/java/com/testingedu/utilsdemo/MultiThread/MyCallable1.java
@@ -1,9 +1,12 @@
 package com.testingedu.utilsdemo.MultiThread;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
+@Slf4j
 public class MyCallable1 {
 
 
@@ -15,6 +18,9 @@ public class MyCallable1 {
         Callable<Integer> callable = new Callable<Integer>() {
             @Override
             public Integer call() throws Exception {
+                for (int i=0;i<=10;i++){
+                log.debug("test="+i);
+                }
                 return 55;
             }
         };
@@ -22,7 +28,7 @@ public class MyCallable1 {
 //      使用FutureTask类来包装Callable对象
         FutureTask<Integer> future = new FutureTask<Integer>(callable);
 
-        new Thread(future, "有返回值的线程").start();//实质上还是以Callable对象来创建并启动线程
+        new Thread(future, "有返回值的线程,此处是线程的名称").start();//实质上还是以Callable对象来创建并启动线程
 
         try {
             System.out.println("子线程的返回值：" + future.get());//get()方法会阻塞，直到子线程执行结束才返回
--- spring-boot-util-demo/src/main/java/com/testingedu/utilsdemo/MultiThread/MyRunnable1.java
+++ spring-boot-util-demo/src/main/java/com/testingedu/utilsdemo/MultiThread/MyRunnable1.java
@@ -5,7 +5,7 @@ public class MyRunnable1 implements Runnable {
     public void run() {
         try {
             Thread.sleep(1000);
-            for (int i = 0; i < 1; i++) {
+            for (int i = 0; i < 11; i++) {
                 System.out.println("myRunnable1:" + i);
             }
         } catch (InterruptedException e) {
