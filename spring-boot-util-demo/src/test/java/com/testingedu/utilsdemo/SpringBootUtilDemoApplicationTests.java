58afeff33875f567ca63eee0eb3909ea0333f929
siyujing <siyujing@boxfish.cn>
2018-04-28 18:33:06 +0800

    no message

--- spring-boot-util-demo/src/test/java/com/testingedu/utilsdemo/SpringBootUtilDemoApplicationTests.java
+++ spring-boot-util-demo/src/test/java/com/testingedu/utilsdemo/SpringBootUtilDemoApplicationTests.java
@@ -1,21 +1,34 @@
 package com.testingedu.utilsdemo;
 
-import com.testingedu.utilsdemo.App.conf.BeProperties;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Collection;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootUtilDemoApplicationTests {
-    @Autowired
-    BeProperties beProperties;
 
     @Test
     public void contextLoads() {
-        beProperties.show();
+        Multimap<String,String> myMultimap = ArrayListMultimap.create();
+        myMultimap.put("a","a1");
+        myMultimap.put("a","a2");
+        myMultimap.put("b","b1");
+        Map<String, Collection<String>> asMap = myMultimap.asMap();
+        asMap.forEach((key, value)->{
+            System.out.println(key+"--"+value);
+        });
+
+        boolean b = myMultimap.containsEntry("a", "a1");
+        System.out.println(b);
+        Collection<Map.Entry<String, String>> entries = myMultimap.entries();
+
     }
 
 }
