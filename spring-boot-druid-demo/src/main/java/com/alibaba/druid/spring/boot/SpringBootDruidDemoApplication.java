9c85d7862c468a96854693a49b9199107c11bb7f
siyujing <siyujing@boxfish.cn>
2018-05-04 18:41:47 +0800

    [A] druid

---
+++ spring-boot-druid-demo/src/main/java/com/alibaba/druid/spring/boot/SpringBootDruidDemoApplication.java
@@ -0,0 +1,12 @@
+package com.alibaba.druid.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDruidDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDruidDemoApplication.class, args);
+	}
+}
