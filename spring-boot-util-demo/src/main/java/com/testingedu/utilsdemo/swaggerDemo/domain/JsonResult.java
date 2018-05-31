81a2957b2eee11a5a5bdbedc6ff3ee82b290ed68
syj <siyujing@boxfish.cn>
2018-05-31 14:08:03 +0800

    [A] 新增swagger2

---
+++ spring-boot-util-demo/src/main/java/com/testingedu/utilsdemo/swaggerDemo/domain/JsonResult.java
@@ -0,0 +1,11 @@
+package com.testingedu.utilsdemo.swaggerDemo.domain;
+
+import lombok.Data;
+
+@Data
+public class JsonResult {
+
+    private String status = null;
+
+    private Object result = null;
+}
