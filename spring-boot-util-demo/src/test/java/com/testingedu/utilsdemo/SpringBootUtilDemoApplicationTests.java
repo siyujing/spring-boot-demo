e7ed26b3bac0bcec456218e46e3f01ae927ea92b
syj <1097104885@qq.com>
2018-09-11 13:51:47 +0800

    [A]新增命令和职责链的设计模式

--- spring-boot-util-demo/src/test/java/com/testingedu/utilsdemo/SpringBootUtilDemoApplicationTests.java
+++ spring-boot-util-demo/src/test/java/com/testingedu/utilsdemo/SpringBootUtilDemoApplicationTests.java
@@ -1,6 +1,8 @@
 package com.testingedu.utilsdemo;
 
 import com.testingedu.utilsdemo.MultiThread.synchrized_suo.NotifyNotifyAll;
+import com.testingedu.utilsdemo.线程间通信.方法一.MethodOne;
+import com.testingedu.utilsdemo.线程间通信.方法一.MethodTwo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,12 +14,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SpringBootUtilDemoApplicationTests {
 
     @Autowired
-    private NotifyNotifyAll notifyNotifyAll;
+    private MethodTwo methodTwo;
 
     @Test
-    public void test() throws InterruptedException {
-//        notifyNotifyAll.test();
-//        Thread.sleep(10000);
+    public void test() {
+        final Runnable runnable1 = methodTwo.newThroadOne();
+        final Runnable runnable2 = methodTwo.newThroadTwo();
+        Thread thread = new Thread(runnable1);
+        thread.start();
+
+        Thread thread2 = new Thread(runnable2);
+        thread2.start();
     }
 
 }
